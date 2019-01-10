8d6afdb278c166fd0dc10f9c3a271d672aff75cb
Stefan Kruger <stefan.kruger@gmail.com>
2019-01-10 12:28:43 -0800

    day16 p2

--- day16.py
+++ day16.py
@@ -8,102 +8,139 @@ from collections import defaultdict
 import json
 import re
 
-def addr(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] + register[B]
-    return result
+class Instr:
 
-def addi(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] + B
-    return result
+    @staticmethod
+    def addr(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] + register[B]
+        return result
 
-def mulr(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] * register[B]
-    return result
+    @staticmethod
+    def addi(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] + B
+        return result
 
-def muli(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] * B
-    return result
+    @staticmethod
+    def mulr(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] * register[B]
+        return result
 
-def banr(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] & register[B]
+    @staticmethod
+    def muli(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] * B
+        return result
 
-    return result
+    @staticmethod
+    def banr(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] & register[B]
 
-def bani(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] & B
-    return result
+        return result
 
-def borr(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] | register[B]
-    return result
+    @staticmethod
+    def bani(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] & B
+        return result
 
-def bori(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = register[A] | B
-    return result
+    @staticmethod
+    def borr(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] | register[B]
+        return result
 
-def setr(register, instr):
-    (_opcode, A, _B, C) = instr
-    result = copy(register)
-    result[C] = register[A]
-    return result
+    @staticmethod
+    def bori(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = register[A] | B
+        return result
 
-def seti(register, instr):
-    (_opcode, A, _B, C) = instr
-    result = copy(register)
-    result[C] = A
-    return result
+    @staticmethod
+    def setr(register, instr):
+        (_opcode, A, _B, C) = instr
+        result = copy(register)
+        result[C] = register[A]
+        return result
 
-def gtir(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = int(A > register[B])
-    return result
+    @staticmethod
+    def seti(register, instr):
+        (_opcode, A, _B, C) = instr
+        result = copy(register)
+        result[C] = A
+        return result
 
-def gtri(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = int(register[A] > B)
-    return result
+    @staticmethod
+    def gtir(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = int(A > register[B])
+        return result
 
-def gtrr(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = int(register[A] > register[B])
-    return result
+    @staticmethod
+    def gtri(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = int(register[A] > B)
+        return result
 
-def eqir(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = int(A == register[B])
-    return result
+    @staticmethod
+    def gtrr(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = int(register[A] > register[B])
+        return result
 
-def eqri(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = int(register[A] == B)
-    return result
+    @staticmethod
+    def eqir(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = int(A == register[B])
+        return result
 
-def eqrr(register, instr):
-    (_opcode, A, B, C) = instr
-    result = copy(register)
-    result[C] = int(register[A] == register[B])
-    return result
+    @staticmethod
+    def eqri(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = int(register[A] == B)
+        return result
+
+    @staticmethod
+    def eqrr(register, instr):
+        (_opcode, A, B, C) = instr
+        result = copy(register)
+        result[C] = int(register[A] == register[B])
+        return result
+
+    @staticmethod
+    def symtable():
+         return {
+            "addr": Instr.addr, "addi": Instr.addi, 
+            "mulr": Instr.mulr, "muli": Instr.muli, 
+            "banr": Instr.banr, "bani": Instr.bani, 
+            "borr": Instr.borr, "bori": Instr.bori, 
+            "setr": Instr.setr, "seti": Instr.seti, 
+            "gtir": Instr.gtir, "gtri": Instr.gtri, "gtrr": Instr.gtrr, 
+            "eqri": Instr.eqri, "eqir": Instr.eqir, "eqrr": Instr.eqrr
+        }
+
+def analyse_sample(before, instruction, after):
+    return {
+        opname
+        for opname, op in Instr.symtable().items()
+        if after == op(before, instruction)
+    }
 
 def read_data(filename="data/input16-1.data"):
     with open(filename) as f:
@@ -121,45 +158,44 @@ def parse_data(lines):
         result.append(op)
 
     return result
+
+def parse_data_part2(lines):
+    """
+    opcode A B C
+    """
+    return [
+        [int(n) for n in re.findall(r"-?\d+", line)]
+        for line in lines
+    ]
             
-def analyse_sample(before, instruction, after):
-    instructions = {
-        # Addition
-        "addr": addr, 
-        "addi": addi, 
+def find_opcodes(samples):
+    by_opcode = defaultdict(set)
 
-        # Multiplication
-        "mulr": mulr, 
-        "muli": muli, 
+    for before, instruction, after in samples:
+        by_opcode[instruction[0]].update(analyse_sample(before, instruction, after))
 
-        # Bit-wise AND
-        "banr": banr, 
-        "bani": bani, 
+    singles = set()
 
-        # Bit-wise OR
-        "borr": borr, 
-        "bori": bori, 
+    while True:
+        before = len(singles)
+        for _op, match in by_opcode.items():
+            if len(match) > 1:
+                match -= singles
+            
+            if len(match) == 1:
+                singles.add(next(iter(match)))
 
-        # Assignment
-        "setr": setr, 
-        "seti": seti, 
+        if len(singles) == before:
+            break
 
-        # Greater-than testing
-        "gtir": gtir, 
-        "gtri": gtri, 
-        "gtrr": gtrr, 
+    sym = Instr.symtable()
 
-        # Equality testing
-        "eqri": eqri, 
-        "eqir": eqir, 
-        "eqrr": eqrr
-    }
     return {
-        opname
-        for opname, op in instructions.items()
-        if after == op(before, instruction)
+        opcode: sym[next(iter(opname_set))]
+        for opcode, opname_set in by_opcode.items()
     }
-    
+
+
 if __name__ == "__main__":
     lines = read_data()
     samples = parse_data(lines)
@@ -171,5 +207,16 @@ if __name__ == "__main__":
     
     print(f"Part1: {count}")
 
+    opcodes = find_opcodes(samples)
+ 
+    lines = read_data(filename="data/input16-2.data")
+    instructions = parse_data_part2(lines)
+
+    register = [0, 0, 0, 0]
+    for instr in instructions:
+        op = opcodes[instr[0]]
+        register = op(register, instr)
+
+    print(f"Part2: {register[0]}")
 
 
