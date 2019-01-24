e5c71fcbcd1f470f1d6d804d49731a98410f5052
Stefan Kruger <stefan.kruger@gmail.com>
2019-01-24 15:23:30 +0000

    part 2

--- day19.py
+++ day19.py
@@ -2,133 +2,56 @@
 day 19 of Advent of Code 2018
 by Stefan Kruger
 """
-
-from copy import copy
+import time
+import re
 
 class Instr:
 
     @staticmethod
     def addr(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] + register[B]
-        return result
+        (_op, A, B, C) = instr
+        register[C] = register[A] + register[B]
 
     @staticmethod
     def addi(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] + B
-        return result
+        (_op, A, B, C) = instr
+        register[C] = register[A] + B
 
     @staticmethod
     def mulr(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] * register[B]
-        return result
+        (_op, A, B, C) = instr
+        register[C] = register[A] * register[B]
 
     @staticmethod
     def muli(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] * B
-        return result
-
-    @staticmethod
-    def banr(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] & register[B]
-        return result
-
-    @staticmethod
-    def bani(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] & B
-        return result
-
-    @staticmethod
-    def borr(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] | register[B]
-        return result
-
-    @staticmethod
-    def bori(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = register[A] | B
-        return result
+        (_op, A, B, C) = instr
+        register[C] = register[A] * B
 
     @staticmethod
     def setr(register, instr):
-        (_opcode, A, _B, C) = instr
-        result = copy(register)
-        result[C] = register[A]
-        return result
+        (_op, A, B, C) = instr
+        register[C] = register[A]
 
     @staticmethod
     def seti(register, instr):
-        (_opcode, A, _B, C) = instr
-        result = copy(register)
-        result[C] = A
-        return result
-
-    @staticmethod
-    def gtir(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = int(A > register[B])
-        return result
-
-    @staticmethod
-    def gtri(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = int(register[A] > B)
-        return result
+        (_op, A, B, C) = instr
+        register[C] = A
 
     @staticmethod
     def gtrr(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = int(register[A] > register[B])
-        return result
-
-    @staticmethod
-    def eqir(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = int(A == register[B])
-        return result
-
-    @staticmethod
-    def eqri(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = int(register[A] == B)
-        return result
+        (_op, A, B, C) = instr
+        register[C] = int(register[A] > register[B])
 
     @staticmethod
     def eqrr(register, instr):
-        (_opcode, A, B, C) = instr
-        result = copy(register)
-        result[C] = int(register[A] == register[B])
-        return result
+        (_op, A, B, C) = instr
+        register[C] = int(register[A] == register[B])
 
     @staticmethod
     def symtable():
          return {
-            "addr": Instr.addr, "addi": Instr.addi, 
-            "mulr": Instr.mulr, "muli": Instr.muli, 
-            "banr": Instr.banr, "bani": Instr.bani, 
-            "borr": Instr.borr, "bori": Instr.bori, 
-            "setr": Instr.setr, "seti": Instr.seti, 
-            "gtir": Instr.gtir, "gtri": Instr.gtri, "gtrr": Instr.gtrr, 
-            "eqri": Instr.eqri, "eqir": Instr.eqir, "eqrr": Instr.eqrr
+            "addr": Instr.addr, "addi": Instr.addi, "mulr": Instr.mulr, "muli": Instr.muli, 
+            "setr": Instr.setr, "seti": Instr.seti, "gtrr": Instr.gtrr, "eqrr": Instr.eqrr
         }
 
 def read_data(filename="data/input19.data"):
@@ -136,9 +59,6 @@ def read_data(filename="data/input19.data"):
         return f.read().splitlines()
 
 def parse_data(lines):
-    """
-    opcode A B C
-    """
     ip = int(lines[0][-1])
     code = []
     for line in lines[1:]:
@@ -146,33 +66,61 @@ def parse_data(lines):
     
     return ip, code
 
+
 if __name__ == "__main__":
     lines = read_data()
-    # lines = [
-    #     "#ip 0",
-    #     "seti 5 0 1",
-    #     "seti 6 0 2",
-    #     "addi 0 1 0",
-    #     "addr 1 2 3",
-    #     "setr 1 0 0",
-    #     "seti 8 0 4",
-    #     "seti 9 0 5"
-    # ]
-
+ 
     ip_reg, instructions = parse_data(lines)
 
-    register = [0, 0, 0, 0, 0, 0]
     ip = 0
-    opcodes = Instr.symtable()
+    register = [0, 0, 0, 0, 0, 0]
+    opcodes = Instr.symtable() 
+
     while ip < len(instructions):
+
+        # Fetch
         instr = instructions[ip]
         op = opcodes[instr[0]]
+
+        # Copy IP into bound register
         register[ip_reg] = ip
-        print(ip, register, instr[0], " ", end="")
-        register = op(register, instr)
-        ip = register[ip_reg]
-        ip += 1
-        print(register)
+
+        # Execute instruction
+        op(register, instr)
         
+        # Copy IP from bound register
+        ip = register[ip_reg]
 
+        # Next instruction
+        ip += 1        
 
+    print(f"Part1: {register[0]}")
+
+    # Part2: starting with register = [1, 0, 0, 0, 0, 0] is a very
+    # different proposition. There are two nested loops, the key one
+    # the sectin at ip=3-11. In all it basically looks like this:
+    #
+    # reg = [0, 1, 10551432, 1, 1, 3] -- state when we enter loop
+    # 
+    # while reg[3] <= reg[2]:
+    #     reg[1] = 1
+    #     while reg[1] <= reg[2]:
+    #         if reg[3] * reg[1] == reg[2]:
+    #             reg[0] += reg[3]
+    #         reg[1] += 1
+    #     reg[3] += 1
+    # 
+    # so that's two nested loops to 10_551_432 -- too much to run.
+    #
+    # However, we can optimise away the inner loop completely, as shown
+    # below, as it only modifies register[0] whenever register[3]
+    # divides register[2]:
+
+    register = [0, 1, 10551432, 1, 1, 3]
+
+    while register[3] <= register[2]:
+        if register[2] / register[3] == register[2] // register[3]:
+            register[0] += register[3]
+        register[3] += 1
+
+    print(f"Part2: {register[0]}")
