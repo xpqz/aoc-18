8dcc6a3e03a1382c381d63dacc3bc33a0de5f644
Stefan kruger <stefan.kruger@gmail.com>
2019-02-12 12:10:09 +0000

    days 23 and 23 parts 2

--- day22.py
+++ day22.py
@@ -2,6 +2,10 @@
 day 22 of Advent of Code 2018
 by Stefan Kruger
 """
+from functools import lru_cache
+import heapq
+import math
+
 def erosion_level(t, depth):
     return (t + depth) % 20183
 
@@ -18,11 +22,31 @@ def geo_index(pos, target, depth):
                 vals[this] = this[1] * 48271
             else:
                 vals[this] = (
-                    (vals[(this[0]-1, this[1])] + depth) % 20183 * 
+                    (vals[(this[0]-1, this[1])] + depth) % 20183 *
                     (vals[(this[0], this[1]-1)] + depth) % 20183
                 )
 
-    return vals[pos]    
+    return vals[pos]
+
+@lru_cache(None)
+def region(x, y, target, depth):
+    gi = geo_index((x, y), target, depth)
+    return erosion_level(gi, depth) % 3
+
+def neighbours(x, y, tool, target, depth):
+    """
+    Return valid neighbours with cost, given hefted tool
+    """
+    for delta in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        xpos = x+delta[0]
+        ypos = y+delta[1]
+        if xpos < 0 or ypos < 0:
+            continue
+        region_tool = region(xpos, ypos, target, depth)
+        for new_tool in range(3):
+            if region_tool != new_tool:
+                cost = 8 if tool != new_tool else 1
+                yield xpos, ypos, new_tool, cost
 
 if __name__ == "__main__":
 
@@ -34,9 +58,33 @@ if __name__ == "__main__":
         for x in range(0, target[0]+1):
             if (x, y) in {(0, 0), target}:
                 continue
-            gi = geo_index((x, y), target, depth)
-            risk += erosion_level(gi, depth) % 3
-                
+            risk += region(x, y, target, depth)
+
     print(f"Part1: {risk}")
 
+    # Part 2: a breadth-first search, with the tooling constraints.
+    # Relying on tool == region type == risk.
 
+    target_tool = (target[0], target[1], 1)
+
+    # (time, xpos, ypos, tool)
+    frontier = [(0, 0, 0, 1)]
+    result = {(0, 0, 1): 0}
+    while frontier:
+        (t, x, y, tool) = heapq.heappop(frontier)
+        current = (x, y, tool)
+        if current == target_tool:
+            print(f"Part2: {t-1}")
+            break
+
+        if result.get(current, math.inf) < t:
+            continue
+
+        # As the graph is 'unlimited', we need a heuristic cut-off
+        if x > target[0] * 2 or y > target[1] * 2:
+            continue
+
+        for xpos, ypos, new_tool, cost in neighbours(x, y, tool, target, depth):
+            if t+cost < result.get((xpos, ypos, new_tool), math.inf):
+                result[(xpos, ypos, new_tool)] = t+cost
+                heapq.heappush(frontier, (t+cost, xpos, ypos, new_tool))
--- day23.py
+++ day23.py
@@ -1,8 +1,14 @@
 
+"""
+day 23 of Advent of Code 2018
+by Stefan Kruger
+"""
+
 import re
 import math
 from dataclasses import dataclass
 from typing import Tuple
+from queue import PriorityQueue
 
 @dataclass
 class Bot:
@@ -30,19 +36,16 @@ def parse_data(lines):
     return (strongest, data)
 
 def manhattan_distance(a, b):
-    assert len(a.pos) == len(b.pos)
     distance = 0
-    for i in range(len(a.pos)):
-        distance += abs(b.pos[i]-a.pos[i])
+    for i in range(len(a)):
+        distance += abs(b[i]-a[i])
 
     return distance
-
 
 if __name__ == "__main__":
     lines = read_data()
     (strongest, data) = parse_data(lines)
 
-    print(strongest.pos, " ", strongest.radius)
     reachable = 0
     for p in data:
         d = manhattan_distance(strongest, p)
@@ -50,3 +53,33 @@ if __name__ == "__main__":
             reachable += 1
 
     print(f"Part1: {reachable}")
+
+    # This isn't a general solution, but exploits the fact that the data
+    # consists of massively overlapping bots.
+    #
+    # The elegance is blinding, but credit does not belong to me.
+    #
+    #  https://www.reddit.com/r/adventofcode/comments/a8s17l/2018_day_23_solutions/ecdqzdg/
+    #
+    # Consider a 1-D projection of the bots, and find the min and max distance
+    # from the origin.
+    #
+    # Find the point that has the most overlaps.
+    #
+    q = PriorityQueue()
+    for bot in data:
+        d = abs(bot.pos[0]) + abs(bot.pos[1]) + abs(bot.pos[2])
+        q.put((max(0, d - bot.radius), 1))
+        q.put((d + bot.radius + 1, -1))
+
+    count = 0
+    max_count = 0
+    result = 0
+    while not q.empty():
+        dist, e = q.get()
+        count += e
+        if count > max_count:
+            result = dist
+            max_count = count
+
+    print(f"Part2: {result}")
